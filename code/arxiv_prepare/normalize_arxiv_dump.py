6dbf101bb29c80b931ecbb931e294e7ce5efc8ae
Tarek Saier <tareksaier@gmail.com>
2018-10-18 12:31:27 +0200

    make parse routines callable as functions

--- code/arxiv_prepare/normalize_arxiv_dump.py
+++ code/arxiv_prepare/normalize_arxiv_dump.py
@@ -19,13 +19,6 @@ import sys
 import tarfile
 import tempfile
 
-if len(sys.argv) != 3:
-    print(('usage: python3 nomalize_arxiv_dump.py </path/to/dump/dir> </path/t'
-           'o/out/dir>'))
-    sys.exit()
-
-IN_DIR = sys.argv[1]
-OUT_DIR = sys.argv[2]
 MAIN_TEX_PATT = re.compile(r'(\\begin\s*\{\s*document\s*\})', re.I)
 # ^ with capturing parentheses so that the pattern can be used for splitting
 PDF_EXT_PATT = re.compile(r'^\.pdf$', re.I)
@@ -45,11 +38,6 @@ FILTER_PATTS.append(re.compile(r'\$\$.+?\$\$', re.S))
 FILTER_PATTS.append(re.compile(r'\$.+?\$', re.S))
 FILTER_PATTS.append(re.compile(r'\\\(.+?\\\)', re.S))
 FILTER_PATTS.append(re.compile(r'\\\[.+?\\\]', re.S))
-
-
-def log(msg):
-    with open(os.path.join(OUT_DIR, 'log.txt'), 'a') as f:
-        f.write('{}\n'.format(msg))
 
 
 def read_file(path):
@@ -87,108 +75,126 @@ def remove_math(latex_str):
     return ''.join(parts)
 
 
-if not os.path.isdir(IN_DIR):
-    print('dump directory does not exist')
-    sys.exit()
+def normalize(IN_DIR, OUT_DIR):
+    def log(msg):
+        with open(os.path.join(OUT_DIR, 'log.txt'), 'a') as f:
+            f.write('{}\n'.format(msg))
 
-if not os.path.isdir(OUT_DIR):
-    os.makedirs(OUT_DIR)
+    if not os.path.isdir(IN_DIR):
+        print('dump directory does not exist')
+        return False
 
-for fn in os.listdir(IN_DIR):
-    path = os.path.join(IN_DIR, fn)
-    aid, ext = os.path.splitext(fn)
-    if PDF_EXT_PATT.match(ext):
-        # copy over pdf file as is
-        dest = os.path.join(OUT_DIR, fn)
-        shutil.copyfile(path, dest)
-    elif GZ_EXT_PATT.match(ext):
-        if tarfile.is_tarfile(path):
-            with tempfile.TemporaryDirectory() as tmp_dir_path:
-                # extract archive contents
-                tar = tarfile.open(path)
-                fnames = tar.getnames()
-                tar.extractall(path=tmp_dir_path)
-                # identify main tex file
-                main_tex_path = None
-                ignored_names = []
-                # check .tex files first
-                for tfn in fnames:
-                    if not TEX_EXT_PATT.match(os.path.splitext(tfn)[1]):
-                        ignored_names.append(tfn)
+    if not os.path.isdir(OUT_DIR):
+        os.makedirs(OUT_DIR)
+
+    for fn in os.listdir(IN_DIR):
+        path = os.path.join(IN_DIR, fn)
+        aid, ext = os.path.splitext(fn)
+        if PDF_EXT_PATT.match(ext):
+            # copy over pdf file as is
+            dest = os.path.join(OUT_DIR, fn)
+            shutil.copyfile(path, dest)
+        elif GZ_EXT_PATT.match(ext):
+            if tarfile.is_tarfile(path):
+                with tempfile.TemporaryDirectory() as tmp_dir_path:
+                    # extract archive contents
+                    tar = tarfile.open(path)
+                    fnames = tar.getnames()
+                    tar.extractall(path=tmp_dir_path)
+                    # identify main tex file
+                    main_tex_path = None
+                    ignored_names = []
+                    # check .tex files first
+                    for tfn in fnames:
+                        if not TEX_EXT_PATT.match(os.path.splitext(tfn)[1]):
+                            ignored_names.append(tfn)
+                            continue
+                        tmp_file_path = os.path.join(tmp_dir_path, tfn)
+                        cntnt = read_file(tmp_file_path)
+                        if re.search(MAIN_TEX_PATT, cntnt) is not None:
+                            main_tex_path = tmp_file_path
+                    # try other files
+                    if main_tex_path is None:
+                        for tfn in ignored_names:
+                            tmp_file_path = os.path.join(tmp_dir_path, tfn)
+                            if NON_TEXT_PATT.match(os.path.splitext(tfn)[1]):
+                                continue
+                            try:
+                                cntnt = read_file(tmp_file_path)
+                                if re.search(MAIN_TEX_PATT, cntnt) is not None:
+                                    main_tex_path = tmp_file_path
+                            except:
+                                continue
+                    # give up
+                    if main_tex_path is None:
+                        log(('couldn\'t find main tex file in dump archive {}'
+                             '').format(fn))
                         continue
-                    tmp_file_path = os.path.join(tmp_dir_path, tfn)
-                    cntnt = read_file(tmp_file_path)
-                    if re.search(MAIN_TEX_PATT, cntnt) is not None:
-                        main_tex_path = tmp_file_path
-                # try other files
-                if main_tex_path is None:
-                    for tfn in ignored_names:
+                    # identify bbl file if present
+                    bbl_path = None
+                    for tfn in fnames:
                         tmp_file_path = os.path.join(tmp_dir_path, tfn)
                         if NON_TEXT_PATT.match(os.path.splitext(tfn)[1]):
                             continue
                         try:
                             cntnt = read_file(tmp_file_path)
                             if re.search(MAIN_TEX_PATT, cntnt) is not None:
-                                main_tex_path = tmp_file_path
+                                continue
+                            if BBL_SIGN in cntnt:
+                                bbl_path = tmp_file_path
                         except:
                             continue
-                # give up
-                if main_tex_path is None:
-                    log(('couldn\'t find main tex file in dump archive {}'
-                         '').format(fn))
+                    if bbl_path is None:
+                        latexpand_args = ['latexpand',
+                                          main_tex_path]
+                    else:
+                        latexpand_args = ['latexpand',
+                                          '--expand-bbl',
+                                          bbl_path,
+                                          main_tex_path]
+                    # flatten to single tex file and save
+                    new_tex_fn = '{}.tex'.format(aid)
+                    tmp_dest = os.path.join(tmp_dir_path, new_tex_fn)
+                    out = open(tmp_dest, mode='w')
+                    err = open(os.path.join(OUT_DIR, 'log_latexpand.txt'), 'a')
+                    err.write('\n------------- {} -------------\n'.format(aid))
+                    err.flush()
+                    subprocess.run(latexpand_args, stdout=out, stderr=err,
+                                   cwd=tmp_dir_path)
+                    out.close()
+                    err.close()
+                    # re-read and write to ensure utf-8 b/c latexpand doesn't
+                    # behave
+                    cntnt = read_file(tmp_dest)
+                    if PRE_FILTER_MATH:
+                        cntnt = remove_math(cntnt)
+                    dest = os.path.join(OUT_DIR, new_tex_fn)
+                    with open(dest, mode='w', encoding='utf-8') as f:
+                        f.write(cntnt)
+            else:
+                # extraxt gzipped tex file
+                cntnt = read_gzipped_file(path)
+                if re.search(MAIN_TEX_PATT, cntnt) is None:
+                    log('unexpected content in dump archive {}'.format(fn))
                     continue
-                # identify bbl file if present
-                bbl_path = None
-                for tfn in fnames:
-                    tmp_file_path = os.path.join(tmp_dir_path, tfn)
-                    if NON_TEXT_PATT.match(os.path.splitext(tfn)[1]):
-                        continue
-                    try:
-                        cntnt = read_file(tmp_file_path)
-                        if re.search(MAIN_TEX_PATT, cntnt) is not None:
-                            continue
-                        if BBL_SIGN in cntnt:
-                            bbl_path = tmp_file_path
-                    except:
-                        continue
-                if bbl_path is None:
-                    latexpand_args = ['latexpand',
-                                      main_tex_path]
-                else:
-                    latexpand_args = ['latexpand',
-                                      '--expand-bbl',
-                                      bbl_path,
-                                      main_tex_path]
-                # flatten to single tex file and save
-                new_tex_fn = '{}.tex'.format(aid)
-                tmp_dest = os.path.join(tmp_dir_path, new_tex_fn)
-                out = open(tmp_dest, mode='w')
-                err = open(os.path.join(OUT_DIR, 'log_latexpand.txt'), 'a')
-                err.write('\n------------- {} -------------\n'.format(aid))
-                err.flush()
-                subprocess.run(latexpand_args, stdout=out, stderr=err,
-                               cwd=tmp_dir_path)
-                out.close()
-                err.close()
-                # re-read and write to ensure utf-8 b/c latexpand doesn't
-                # behave
-                cntnt = read_file(tmp_dest)
+                new_fn = '{}.tex'.format(aid)
                 if PRE_FILTER_MATH:
                     cntnt = remove_math(cntnt)
-                dest = os.path.join(OUT_DIR, new_tex_fn)
+                dest = os.path.join(OUT_DIR, new_fn)
                 with open(dest, mode='w', encoding='utf-8') as f:
                     f.write(cntnt)
         else:
-            # extraxt gzipped tex file
-            cntnt = read_gzipped_file(path)
-            if re.search(MAIN_TEX_PATT, cntnt) is None:
-                log('unexpected content in dump archive {}'.format(fn))
-                continue
-            new_fn = '{}.tex'.format(aid)
-            if PRE_FILTER_MATH:
-                cntnt = remove_math(cntnt)
-            dest = os.path.join(OUT_DIR, new_fn)
-            with open(dest, mode='w', encoding='utf-8') as f:
-                f.write(cntnt)
-    else:
-        log('unexpected file {} in dump directory'.format(fn))
+            log('unexpected file {} in dump directory'.format(fn))
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(('usage: python3 nomalize_arxiv_dump.py </path/to/dump/dir> </pa'
+               'th/to/out/dir>'))
+        sys.exit()
+    IN_DIR = sys.argv[1]
+    OUT_DIR = sys.argv[2]
+    ret = normalize(IN_DIR, OUT_DIR)
+    if not ret:
+        sys.exit()
--- code/arxiv_prepare/parse_latex_tralics.py
+++ code/arxiv_prepare/parse_latex_tralics.py
@@ -15,128 +15,133 @@ import tempfile
 import uuid
 from lxml import etree
 
-if len(sys.argv) != 3:
-    print(('usage: python3 parse_latex_tralics.py </path/to/in/dir> </path/to/'
-           'out/dir>'))
-    sys.exit()
-
-INCREMENTAL = True
-IN_DIR = sys.argv[1]
-OUT_DIR = sys.argv[2]
 PDF_EXT_PATT = re.compile(r'^\.pdf$', re.I)
 
 
-def log(msg):
-    with open(os.path.join(OUT_DIR, 'log.txt'), 'a') as f:
-        f.write('{}\n'.format(msg))
+def parse(IN_DIR, OUT_DIR, INCREMENTAL=True):
+    def log(msg):
+        with open(os.path.join(OUT_DIR, 'log.txt'), 'a') as f:
+            f.write('{}\n'.format(msg))
 
+    if not os.path.isdir(IN_DIR):
+        print('input directory does not exist')
+        return False
 
-if not os.path.isdir(IN_DIR):
-    print('input directory does not exist')
-    sys.exit()
+    if not os.path.isdir(OUT_DIR):
+        os.makedirs(OUT_DIR)
 
-if not os.path.isdir(OUT_DIR):
-    os.makedirs(OUT_DIR)
-
-for fn in os.listdir(IN_DIR):
-    path = os.path.join(IN_DIR, fn)
-    aid, ext = os.path.splitext(fn)
-    out_txt_path = os.path.join(OUT_DIR, '{}.txt'.format(aid))
-    if INCREMENTAL and os.path.isfile(out_txt_path):
-        print('{} already in output directory, skipping'.format(aid))
-        continue
-    print(aid)
-    if PDF_EXT_PATT.match(ext):
-        log('skipping file {} (PDF)'.format(fn))
-        continue
-
-    with tempfile.TemporaryDirectory() as tmp_dir_path:
-        tmp_xml_path = os.path.join(tmp_dir_path, '{}.xml'.format(aid))
-        # run latexml
-        tralics_args = ['tralics',
-                        '-silent',
-                        '-noxmlerror',
-                        '-utf8',
-                        '-oe8',
-                        '-entnames=false',
-                        '-nomathml',
-                        '-output_dir={}'.format(tmp_dir_path),
-                        path]
-
-        out = open(os.path.join(OUT_DIR, 'log_tralics.txt'), 'a')
-        err = open(os.path.join(tmp_dir_path, 'tralics_out.txt'), mode='w')
-        out.write('\n------------- {} -------------\n'.format(aid))
-        out.flush()
-        try:
-            subprocess.run(tralics_args, stdout=out, stderr=err, timeout=5)
-        except subprocess.TimeoutExpired as e:
-            print('FAILED {}. skipping'.format(aid))
-            log('\n--- {} ---\n{}\n----------\n'.format(aid, e))
+    for fn in os.listdir(IN_DIR):
+        path = os.path.join(IN_DIR, fn)
+        aid, ext = os.path.splitext(fn)
+        out_txt_path = os.path.join(OUT_DIR, '{}.txt'.format(aid))
+        if INCREMENTAL and os.path.isfile(out_txt_path):
+            print('{} already in output directory, skipping'.format(aid))
             continue
-        out.close()
-        err.close()
-
-        # get mathless plain text from latexml output
-        parser = etree.XMLParser()
-        if not os.path.isfile(tmp_xml_path):
-            print('FAILED {}. skipping'.format(aid))
-            log('\n--- {} ---\n{}\n----------\n'.format(aid,
-                                                        'no tralics output'))
+        print(aid)
+        if PDF_EXT_PATT.match(ext):
+            log('skipping file {} (PDF)'.format(fn))
             continue
-        with open(tmp_xml_path) as f:
+
+        with tempfile.TemporaryDirectory() as tmp_dir_path:
+            tmp_xml_path = os.path.join(tmp_dir_path, '{}.xml'.format(aid))
+            # run latexml
+            tralics_args = ['tralics',
+                            '-silent',
+                            '-noxmlerror',
+                            '-utf8',
+                            '-oe8',
+                            '-entnames=false',
+                            '-nomathml',
+                            '-output_dir={}'.format(tmp_dir_path),
+                            path]
+
+            out = open(os.path.join(OUT_DIR, 'log_tralics.txt'), 'a')
+            err = open(os.path.join(tmp_dir_path, 'tralics_out.txt'), mode='w')
+            out.write('\n------------- {} -------------\n'.format(aid))
+            out.flush()
             try:
-                tree = etree.parse(f, parser)
-            except etree.XMLSyntaxError as e:
+                subprocess.run(tralics_args, stdout=out, stderr=err, timeout=5)
+            except subprocess.TimeoutExpired as e:
                 print('FAILED {}. skipping'.format(aid))
                 log('\n--- {} ---\n{}\n----------\n'.format(aid, e))
                 continue
-        etree.strip_elements(tree, 'formula', with_tail=False)
+            out.close()
+            err.close()
 
-        # processing of citation markers
-        bibitems = tree.xpath('//bibitem')
-        bibkey_map = {}
-        bibitem_map = {}
-
-        for bi in bibitems:
-            uid = str(uuid.uuid4())
-            local_key = bi.get('id')
-            bibkey_map[local_key] = uid
-            containing_p = bi.getparent()
-            text = etree.tostring(containing_p,
-                                  encoding='unicode',
-                                  method='text')
-            text = re.sub('\s+', ' ', text).strip()
-            bibitem_map[uid] = text
-
-        citations = tree.xpath('//cit')
-        for cit in citations:
-            elem = cit.find('ref')
-            if elem is None:
-                log(('WARNING: cite element in {} contains no ref element'
-                     '').format(aid))
+            # get mathless plain text from latexml output
+            parser = etree.XMLParser()
+            if not os.path.isfile(tmp_xml_path):
+                print('FAILED {}. skipping'.format(aid))
+                log(('\n--- {} ---\n{}\n----------\n'
+                     '').format(aid, 'no tralics output'))
                 continue
-            ref = elem.get('target')
-            replace_text = ''
-            if ref in bibkey_map:
-                marker = '{{{{cite:{}}}}}'.format(bibkey_map[ref])
-                replace_text += marker
-            else:
-                log(('WARNING: unmatched bibliography key {} for doc {}'
-                     '').format(ref, aid))
-            if cit.tail:
-                cit.tail = replace_text + cit.tail
-            else:
-                cit.tail = replace_text
-        # /processing of citation markers
-        etree.strip_elements(tree, 'Bibliography', with_tail=False)
-        etree.strip_elements(tree, 'bibitem', with_tail=False)
-        etree.strip_elements(tree, 'cit', with_tail=False)
-        etree.strip_tags(tree, '*')
-        tree_str = etree.tostring(tree, encoding='unicode', method='text')
-        # tree_str = re.sub('\s+', ' ', tree_str).strip()
+            with open(tmp_xml_path) as f:
+                try:
+                    tree = etree.parse(f, parser)
+                except etree.XMLSyntaxError as e:
+                    print('FAILED {}. skipping'.format(aid))
+                    log('\n--- {} ---\n{}\n----------\n'.format(aid, e))
+                    continue
+            etree.strip_elements(tree, 'formula', with_tail=False)
 
-        out_map_path = os.path.join(OUT_DIR, '{}_map.json'.format(aid))
-        with open(out_txt_path, 'w') as f:
-            f.write(tree_str)
-        with open(out_map_path, 'w') as f:
-            f.write(json.dumps(bibitem_map))
+            # processing of citation markers
+            bibitems = tree.xpath('//bibitem')
+            bibkey_map = {}
+            bibitem_map = {}
+
+            for bi in bibitems:
+                uid = str(uuid.uuid4())
+                local_key = bi.get('id')
+                bibkey_map[local_key] = uid
+                containing_p = bi.getparent()
+                text = etree.tostring(containing_p,
+                                      encoding='unicode',
+                                      method='text')
+                text = re.sub('\s+', ' ', text).strip()
+                bibitem_map[uid] = text
+
+            citations = tree.xpath('//cit')
+            for cit in citations:
+                elem = cit.find('ref')
+                if elem is None:
+                    log(('WARNING: cite element in {} contains no ref element'
+                         '').format(aid))
+                    continue
+                ref = elem.get('target')
+                replace_text = ''
+                if ref in bibkey_map:
+                    marker = '{{{{cite:{}}}}}'.format(bibkey_map[ref])
+                    replace_text += marker
+                else:
+                    log(('WARNING: unmatched bibliography key {} for doc {}'
+                         '').format(ref, aid))
+                if cit.tail:
+                    cit.tail = replace_text + cit.tail
+                else:
+                    cit.tail = replace_text
+            # /processing of citation markers
+            etree.strip_elements(tree, 'Bibliography', with_tail=False)
+            etree.strip_elements(tree, 'bibitem', with_tail=False)
+            etree.strip_elements(tree, 'cit', with_tail=False)
+            etree.strip_tags(tree, '*')
+            tree_str = etree.tostring(tree, encoding='unicode', method='text')
+            # tree_str = re.sub('\s+', ' ', tree_str).strip()
+
+            out_map_path = os.path.join(OUT_DIR, '{}_map.json'.format(aid))
+            with open(out_txt_path, 'w') as f:
+                f.write(tree_str)
+            with open(out_map_path, 'w') as f:
+                f.write(json.dumps(bibitem_map))
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print(('usage: python3 parse_latex_tralics.py </path/to/in/dir> </path'
+               '/to/out/dir>'))
+        sys.exit()
+    IN_DIR = sys.argv[1]
+    OUT_DIR = sys.argv[2]
+    ret = parse(IN_DIR, OUT_DIR, INCREMENTAL=True)
+    if not ret:
+        sys.exit()

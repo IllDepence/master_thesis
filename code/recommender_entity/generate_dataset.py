76f2e77f558bb050e6219bc634755cd76ac68db9
Tarek Saier <tareksaier@gmail.com>
2018-11-13 23:11:05 +0100

    add notes

--- code/recommender_entity/generate_dataset.py
+++ code/recommender_entity/generate_dataset.py
@@ -1,4 +1,4 @@
-""" Generate datasets from a parsed, matched and annotated arXiv dump
+""" Generate datasets from a parsed and matched arXiv dump
 """
 
 import json
@@ -13,8 +13,33 @@ from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from db_model import Base, Bibitem, BibitemArxivIDMap
 
-CITE_PATT = re.compile((r'\{\{cite:[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB]'
-                         '[0-9A-F]{3}-[0-9A-F]{12}\}\}'), re.I)
+CITE_PATT = re.compile((r'\{\{cite:([0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}'
+                         '-[89AB][0-9A-F]{3}-[0-9A-F]{12})\}\}'), re.I)
+
+
+def find_adjacent_citations(adfix, uuid_aid_map, backwards=False):
+    """ Given text after or before a citation, find all directly adjacent
+        citations.
+    """
+
+    if backwards:
+        perimeter = adfix[-50:]
+    else:
+        perimeter = adfix[:50]
+    match = CITE_PATT.search(perimeter)
+    if not match:
+        return []
+    uuid = match.group(1)
+    if uuid not in uuid_aid_map:
+        return []
+    aid = uuid_aid_map[uuid]
+    margin = perimeter.index(match.group(0))
+    if backwards:
+        adfix = adfix[:-(50-margin)]
+    else:
+        adfix = adfix[45+margin:]
+    moar = find_adjacent_citations(adfix, uuid_aid_map, backwards=backwards)
+    return [aid] + moar
 
 
 def generate(in_dir, db_uri=None, context_size=100, min_contexts=4,
@@ -39,73 +64,104 @@ def generate(in_dir, db_uri=None, context_size=100, min_contexts=4,
     session = DBSession()
 
     print('querying DB')
-    sub = session.query(BibitemArxivIDMap.arxiv_id).\
-                subquery()
-    res = session.query(Bibitem, BibitemArxivIDMap).\
+    have_global_id = session.query(BibitemArxivIDMap.arxiv_id).\
+                        subquery()
+    bibitems = session.query(Bibitem, BibitemArxivIDMap).\
                 filter(Bibitem.uuid == BibitemArxivIDMap.uuid).\
-                filter(BibitemArxivIDMap.arxiv_id.in_(sub)).\
+                filter(BibitemArxivIDMap.arxiv_id.in_(have_global_id)).\
                 all()
     print('merging bibitems')
-    cited_docs = {}
-    for bibitem in res:
+    cited_docs_pre = {}
+    uuid_aid_map = {}
+    for bibitem in bibitems:
         aid = bibitem.BibitemArxivIDMap.arxiv_id
-        if aid not in cited_docs:
+        uuid = bibitem.Bibitem.uuid
+        uuid_aid_map[uuid] = aid
+        in_doc = bibitem.Bibitem.in_doc
+        if aid not in cited_docs_pre:
+            cited_docs_pre[aid] = {}
+        if in_doc not in cited_docs_pre[aid]:
+            cited_docs_pre[aid][in_doc] = []
+        cited_docs_pre[aid][in_doc].append(uuid)
+    print('checking merging results')
+    cited_docs = {}
+    for aid, doc_dict in cited_docs_pre.items():
+        # for evaluation we *need* at least 2 documents containing citation
+        # contexts (in order to perform a per doc test/train split)
+        if len(doc_dict) > 1:
             cited_docs[aid] = []
-        cited_docs[aid].append({
-            'uuid': bibitem.Bibitem.uuid,
-            'in_doc': bibitem.Bibitem.in_doc
-            })
+            for in_doc, uuid_list in doc_dict.items():
+                cited_docs[aid].append({
+                    'uuid': uuid_list[0],  # uuid_list should always be len. 1
+                    'in_doc': in_doc
+                    })
     print('going through docs')
     contexts = []
     for aid, doc_list in cited_docs.items():
         tmp_list = []
+        num_docs = 0
         for doc in doc_list:
-            fn = '{}.txt'.format(doc['in_doc'])
+            in_doc = doc['in_doc']
+            fn = '{}.txt'.format(in_doc)
             text_file = os.path.join(in_dir, fn)
             with open(text_file) as f:
                 text = f.read()
             marker = '{{{{cite:{}}}}}'.format(doc['uuid'])
+            marker_found = False
             for m in re.finditer(marker, text):
                 margin = int(context_size/2)
                 idx = m.start()
                 edx = m.end()
                 pre = text[:idx]
                 post = text[edx:]
-                # TODO:
-                # - treat citation markers directly next to each other
-                #   speacially (e.g. adding extra markers also to CSV
-                #   e.g. "arxiv:honmono,[arxiv:also|arxiv:metoo],lorem ipsum ."
+                adj_pre = find_adjacent_citations(pre, uuid_aid_map,
+                                                  backwards=True)
+                adj_post = find_adjacent_citations(post, uuid_aid_map)
+                adjacent_citations = adj_pre + adj_post
+                pre = re.sub(CITE_PATT, '', pre)
+                post = re.sub(CITE_PATT, '', post)
+
                 # - figure out which DBpedia annotations are within the chosen
                 #   citation context
+                #       - similar-ish to find_adjacent_citations function:
+                #           - go in each direction, thereby
+                #             count words (up to <margin>)
+                #             jump citations
+                #             keep count of total number of characters passed
+                #           - based on resulting, larger windows size,
+                #             determine contained annotations
                 # - make features out of those entities
                 # - make special features in case of <NE>[]
                 # - mby also POS tagging then <preposition>[] special treatment
                 #   etc.
-                #
-                # pre = re.sub(CITE_PATT, '', pre)
-                # post = re.sub(CITE_PATT, '', post)
-                # # heuristic pre-cutting (10 times average word length)
-                # pre = pre[-margin*6*10:]
-                # post = post[:margin*6*10]
-                # if preprocess:
-                #     pre, post = preprocess_documents([pre, post])
-                # else:
-                #     custom_filter = [strip_punctuation, strip_multiple_whitespaces]
-                #     pre = preprocess_string(pre, custom_filter)
-                #     post = preprocess_string(post, custom_filter)
-                # placeholder = ''
-                # if with_placeholder:
-                #     placeholder = ' [] '
-                # context = '{}{}{}'.format(' '.join(pre[-margin:]),
-                #                           placeholder,
-                #                           ' '.join(post[:margin]))
-                # tmp_list.append((aid, context))
-        if len(tmp_list) >= min_contexts:
+
+                # heuristic pre-cutting (10 times average word length)
+                pre = pre[-margin*6*10:]
+                post = post[:margin*6*10]
+                if preprocess:
+                    pre, post = preprocess_documents([pre, post])
+                else:
+                    custom_filter = [strip_punctuation,
+                                     strip_multiple_whitespaces]
+                    pre = preprocess_string(pre, custom_filter)
+                    post = preprocess_string(post, custom_filter)
+                placeholder = ''
+                if with_placeholder:
+                    placeholder = ' [] '
+                context = '{}{}{}'.format(' '.join(pre[-margin:]),
+                                          placeholder,
+                                          ' '.join(post[:margin]))
+                adj_cit_str = '[{}]'.format('|'.join(adjacent_citations))
+                tmp_list.append([aid, adj_cit_str, in_doc, context])
+                marker_found = True
+            if marker_found:
+                num_docs += 1
+        if len(tmp_list) >= min_contexts and num_docs > 1:
             contexts.extend(tmp_list)
     print(len(contexts))
     with open('items.csv', 'w') as f:
-        for item in contexts:
-            line = '{},{}\n'.format(item[0], item[1])
+        for vals in contexts:
+            line = '{}\n'.format(','.join(vals))
             f.write(line)
 
 if __name__ == '__main__':

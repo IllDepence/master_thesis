11ab179af43d8471db571b2371dee8094cf17aa4
Tarek Saier <tareksaier@gmail.com>
2018-11-19 15:39:28 +0100

    analyze maker pos. relative to NEL annots

--- code/arxiv_misc/citation_positions.py
+++ code/arxiv_misc/citation_positions.py
@@ -43,15 +43,31 @@ def sent_pos(in_dir):
             continue
         with open(path) as f:
             text = f.read()
+        annot_fn = '{}_annot.json'.format(aid)
+        annot_path = os.path.join(in_dir, annot_fn)
+        if not os.path.isfile(annot_path):
+            continue
+        with open(annot_path) as f:
+            annots = json.load(f)
 
         marker = ' \u241F '
-        text = re.sub(CITE_MULTI_PATT, marker, text)
-        text = re.sub(QUOTE_PATT, ' {}.'.format(marker), text)
-
-        sentences = tokenizer.tokenize(text)
-        doc_len = len(sentences)
+        doc_len = len(text)
         # ↓ word wise
-        for sent_idx, sentence in enumerate(sentences):
+        for sent_idx, sent_edx in tokenizer.span_tokenize(text):
+            sentence_orig = text[sent_idx:sent_edx]
+            sentence = re.sub(CITE_MULTI_PATT, marker, sentence_orig)
+            sentence = re.sub(QUOTE_PATT, ' {}.'.format(marker), sentence)
+            # determine contained annotations
+            annotated_words = []
+            for annot in annots:
+                start = annot[0]
+                end = annot[1]
+                dbp_id = annot[2]
+                annot_len = end - start
+                in_sent_idx = start - sent_idx
+                if start >= sent_idx and end <= sent_edx:
+                    disp = sentence_orig[in_sent_idx:in_sent_idx+annot_len]
+                    annotated_words.append(disp)
             if marker in sentence:
                 doc_pos = 1 - (sent_idx/doc_len)
                 buck_y_idx = math.floor(doc_pos*10)
@@ -63,8 +79,8 @@ def sent_pos(in_dir):
                 sent_tags_str = ' '.join([tup[1] for tup in words])
                 indices = [i for i, tup in enumerate(words)
                            if tup[0] == marker.strip()]
-                if 'JJS' not in sent_tags_str:
-                    continue  # FIXME: currently only looking at superlatives
+                # if 'JJS' not in sent_tags_str:
+                #     continue
                 for word_idx in indices:
                     word = words[word_idx][0]
                     # if word == marker.strip() and \
@@ -83,7 +99,12 @@ def sent_pos(in_dir):
                     #     word_idx+1 < len(words) and \
                     #     'VB' in words[word_idx+1][1]:
 
-                    if word == marker.strip():
+                    # if word == marker.strip():
+
+                    if word == marker.strip() and \
+                        (word_idx > 0 and \
+                         words[word_idx-1][0] in annotated_words and \
+                         words[word_idx-1][1] in ['NNP', 'NNPS']):
                         # print(words)
                         # print('doc pos:  {}'.format((sent_idx/doc_len)))
                         # print('sent pos: {}/{}'.format((word_idx+1),sent_len))
@@ -95,8 +116,8 @@ def sent_pos(in_dir):
                         if buck_x_idx == 10:
                             buck_x_idx = 9
                         buckets[buck_y_idx][buck_x_idx] += 1
-        if file_idx > 1000:
-            break
+        # if file_idx > 1000:
+        #     break
 
         # # ↓ character wise
         # for sent_idx, sentence in enumerate(sentences):

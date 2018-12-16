39c16b96988c2de20277cdf921f0f9a8fdfa6118
Tarek Saier <tareksaier@gmail.com>
2018-12-16 13:13:11 +0100

    clean up bibitem matching code

--- code/arxiv_prepare/match_bibitems_mag.py
+++ code/arxiv_prepare/match_bibitems_mag.py
@@ -1,94 +1,32 @@
 """ Match bibitem strings to MAG IDs
-
-    TODO:
-        - if false positives too high, check authors
 """
 
 import datetime
 import json
 import os
-import pysolr
 import re
 import requests
 import sys
 import unidecode
-from lxml import etree
-from fuzzywuzzy import fuzz
-from random import shuffle
 from sqlalchemy import (create_engine, Column, Integer, String, UnicodeText,
                         Table)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from db_model import (Base, Bibitem, BibitemArxivIDMap, BibitemMAGIDMap,
                       BibitemLinkMap)
 
-DOI_PATT = re.compile(r'10.\d{4,9}/[-._;()/:A-Z0-9]+$', re.I)
-IN_PJ_START_PATT = re.compile(
-    r'^(in)?\s*(the)?\s*(Proceedings|Journal)\s+of', re.I)
-TRANSACTIONS_START_PATT = re.compile(
-    r'^\s*[\w\/]+?\s*(Transactions)\s+on', re.I)
-IN_STH_PATT = re.compile(
-    '(In\s+)?(the\s+)?(Proceedings|Journal|Transactions)\s+(of|on)\s+(the\s+)?\w+',
-    re.I)
-PAGE_VOL_START_PATT = re.compile(
-    r'^\s*,?\s*(pages?|pp|vol(ume)?s?)\.?\s+\d', re.I)
-ARXIV_URL_PATT_END = re.compile(
-    r'arxiv\.org\/[a-z0-9-]{1,10}\/(([a-z0-9-]{1,15}\/)?[\d\.]{5,10}(v\d)?$)',
-    re.I)
+DOI_PATT = re.compile(
+    r'10.\d{4,9}/[-._;()/:A-Z0-9]+$', re.I)
 ARXIV_URL_PATT = re.compile(
-    r'arxiv\.org\/[a-z0-9-]{1,10}\/(([a-z0-9-]{1,15}\/)?[\d\.]{4,9}\d)',
-    re.I)
+    r'arxiv\.org\/[a-z0-9-]{1,10}\/(([a-z0-9-]{1,15}\/)?[\d\.]{4,9}\d)', re.I)
 ARXIV_ID_PATT = re.compile(
     r'arXiv:(([a-z0-9-]{1,15}\/)?[\d\.]{4,9}\d)', re.I)
-NAME_LIST_PATT = re.compile(
-   r'^(([A-ZÀ-ÖØ-öø-ÿ].\s+){1,3}[A-ZÀ-ÖØ-öø-ÿ][a-zÀ-ÖØ-öø-ÿ\'-]+(,?\sand|,)\s+)+',
-   re.I)
-solr = pysolr.Solr('http://localhost:8983/solr/mag_papers/', timeout=600)
-
-
-def send_query(query, debug=False):
-    base_url = 'http://localhost:8983/solr/mag_papers/select?q='
-    try:
-        resp = requests.get(base_url + query)
-    except requests.exceptions.ConnectionError:
-        print('can\'t connect so Solr ... exiting')
-        sys.exit()
-    if resp.status_code != 200:
-        # print('unexpected API response: {}\n{}'.format(resp.status_code,
-        #                                                resp.text))
-        return False
-    resp_json = resp.json()
-    num_results = resp_json.get('response', {}).get('numFound', 0)
-    if num_results < 1:
-        # print('No results')
-        return False
-    docs = resp_json.get('response', {}).get('docs', [{}])
-    if debug:
-        print('Top 10 results:')
-        for doc in docs[:10]:
-            print('        - {}'.format(doc.get('original_title', '')))
-    return docs
-
-
-def send_query_pysolr(title):
-    title = re.sub('[^\w\s\d_]+', ' ', title)
-    title = re.sub('\s+', ' ', title)
-    resp = solr.search('normalized_title:"{}"'.format(title))
-    if len(resp) < 1:
-        cut_resp = []
-        for cutoff in range(1, 6):
-            cut = ' '.join(title.split(' ')[cutoff:])
-            r = solr.search('normalized_title:"{}"'.format(cut))
-            cut_resp.extend(list(r))
-        for cutoff in range(1, 6):
-            cut = ' '.join(title.split(' ')[:-cutoff])
-            r = solr.search('normalized_title:"{}"'.format(cut))
-            cut_resp.extend(list(r))
-    return list(resp)
 
 
 def parscit_parse(text):
+    """ Parse bibitem text using Neural-ParsCit docker container.
+    """
+
     url = 'http://localhost:8000/parscit/parse'
     ret = requests.post(url, json={'string':text})
     if ret.status_code != 200:
@@ -114,6 +52,9 @@ def parscit_parse(text):
 
 
 def clean(s):
+    """ Generic string cleaning
+    """
+
     s = re.sub('[^\w\s]+', '', s)
     s = re.sub('\s+', ' ', s)
     return s.strip().lower()
@@ -136,6 +77,9 @@ def mag_normalize(s):
 
 
 def title_by_doi(given_doi):
+    """ Given a DOI, try to get a work's title using crossref.org
+    """
+
     doi_base_url = 'https://data.crossref.org/'
     doi_headers = {'Accept': 'application/citeproc+json',
                    'User-Agent': ('DoiToTitleScript (working on a master thesi'
@@ -210,15 +154,10 @@ def guess_aps_journal_paper_doi(parscit_terms):
     return False
 
 
-def remove_name_list(text):
-    return NAME_LIST_PATT.sub('', text)
-
-
-def remove_containing_work(text):
-    return IN_STH_PATT.sub('', text)
-
-
 def find_arxiv_id(text):
+    """ Loor for an arXiv ID within the given text.
+    """
+
     match = ARXIV_ID_PATT.search(text)
     if match:
         return match.group(1)
@@ -229,72 +168,10 @@ def find_arxiv_id(text):
     return False
 
 
-def clean_by_segments(text_with_delim):
-    segments = text_with_delim.split('¦')
-    clean_title = ''
-    for seg in segments:
-        if IN_PJ_START_PATT.search(seg):
-            break
-        elif TRANSACTIONS_START_PATT.search(seg):
-            break
-        elif PAGE_VOL_START_PATT.search(seg):
-            break
-        elif ARXIV_URL_PATT_END.search(seg):
-            m = ARXIV_URL_PATT_END.search(seg)
-            if len(m.group(0))/len(seg) > .8:
-                break
-        clean_title += seg
-    return clean_title
-
-
-def check_result(bibitem_string, result_doc):
-    """ For a result doc to pass as fitting require that:
-
-            - the original title is (with some leeway) contained in the bibitem
-              string
+def match(db_uri=None, in_dir=None):
+    """ Match bibitem strings to MAG IDs
     """
 
-    clean_bibitem_title = clean(bibitem_string)
-    clean_result_title = clean(result_doc.get('original_title', False))
-    if not clean_result_title:
-        return False
-    # check for exact title match
-    exact_title = clean_result_title in clean_bibitem_title
-    if exact_title:
-        return True
-    sim = 0
-    if len(clean_bibitem_title) < len(clean_result_title):
-        # just match
-        sim = fuzz.ratio(clean_bibitem_title, clean_result_title)
-    else:
-        # sliding window
-        steps = len(clean_bibitem_title) - len(clean_result_title) + 1
-        for shift in range(steps):
-            window = clean_bibitem_title[shift:shift+len(clean_result_title)]
-            window_sim = fuzz.ratio(window, clean_result_title)
-            sim = max(window_sim, sim)
-    return sim > 94
-
-
-def title_query_words_simple(text):
-    words = [w for w in text.split()]
-    query_string = '%2B'.join(words)
-    return 'original_title:{0}'.format(query_string)
-
-
-def title_query_words(text):
-    with open('stopwords.txt') as f:
-        stop_lines = f.readlines()
-    stop_words = [line.strip() for line in stop_lines]
-    clean_text = re.sub('[^\w\s]+', ' ', text)
-    cleaner_text = re.sub('[0-9]+', '', clean_text)
-    words = [w for w in cleaner_text.split(' ') if
-             len(w) > 2 and w not in stop_words]
-    query_string = '%2B'.join(words)
-    return 'original_title:{0}'.format(query_string)
-
-
-def match(db_uri=None, in_dir=None):
     if not (db_uri or in_dir):
         print('need either DB URI or input directory path')
         return False
@@ -343,7 +220,6 @@ def match(db_uri=None, in_dir=None):
                 'opensearch': 'http://a9.com/-/spec/opensearch/1.1/'}
 
     bibitems_db = session.query(Bibitem).all()
-    shuffle(bibitems_db)
     num_matches = 0
     num_checked = 0
     num_false_positives = 0
@@ -474,19 +350,6 @@ def match(db_uri=None, in_dir=None):
         magdb_total_time += d.total_seconds()
         magdb_total_acc += 1
 
-        # if solr_resps:
-        #     for resp_idx in range(len(solr_resps)):
-        #         t1 = datetime.datetime.now()
-        #         candidate = solr_resps[resp_idx]
-        #         solr_match = check_result(text_orig, candidate)
-        #         t2 = datetime.datetime.now()
-        #         d = t2 - t1
-        #         check_total_time += d.total_seconds()
-        #         check_total_acc += 1
-        #         if solr_match:
-        #             # print('-> matched at result pos. #{}'.format(resp_idx+1))
-        #             solr_resp = candidate
-        #             break
         if mag_paper_db:
             magdb_match = True
             magdb_resp = {'paper_id': mag_paper_db.paperid}
@@ -498,22 +361,6 @@ def match(db_uri=None, in_dir=None):
                 num_by_aid_fail += 1
             if doi_success:
                 num_by_doi_fail += 1
-        # if not solr_match and solr_resps and not 'Phys. Rev.' in text_orig:
-        #     continue
-        #     # check fails
-        #     print('- - - - - - - - - - - - - - - - - - - - - - - - - - -')
-        #     print('in doc: {}'.format(in_doc))
-        #     print('original text: {}'.format(text_orig))
-        #     print('query text:    {}'.format(text))
-        #     print('cleaned:       {}'.format(clean(text)))
-        #     print('found in non of:')
-        #     for resp_idx in range(len(solr_resps)):
-        #         rtext = solr_resps[resp_idx].get('original_title', '')
-        #         print('    {}'.format(rtext))
-        #        print('    ({})\n'.format(clean(rtext)))
-        #     input()
-        # print('.')
-        # continue
 
         if magdb_match:
             num_matches += 1
@@ -536,12 +383,6 @@ def match(db_uri=None, in_dir=None):
                 num_checked += 1
                 if not given_doi.lower() == result_doi.lower():
                     num_false_positives += 1
-                    # print('identified {}'.format(result_doi))
-                    # print('but should\'ve been {}'.format(given_doi))
-                    # input()
-        # print('- - - [{}.{} s] - - -'.format(d.total_seconds(), d.microseconds))
-        # print('enter to continue')
-        # input()
         print('- - - - - - - - - - - - - - - - -')
         print('{}/{}'.format(bi_idx+1, bi_total))
         print('matches: {}'.format(num_matches))

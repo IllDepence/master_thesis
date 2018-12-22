012112c7e81051c27cc3f3d3bc3f4cc37745514a
Tarek Saier <tareksaier@gmail.com>
2018-12-22 14:12:01 +0100

    add FoS annot analysis plots

--- img/mag_fos_annot_conf_vals/plot.py
+++ img/mag_fos_annot_conf_vals/plot.py
@@ -5,31 +5,51 @@ from matplotlib.colors import LogNorm
 
 # with open('stepped_match_counts_rel.json') as f:
 #     stepped_match_counts = json.load(f)
-with open('conf_match_tups.json') as f:
+# with open('conf_match_tups.json') as f:
+with open('conf_match_lvl_trips.json') as f:
     tupls = json.load(f)
 
 threshold = 5
-# plt.ylim(top=120000)
-stepped_match_counts = [[], []]
+# plt.ylim(top=82000)
+# stepped_match_counts = [[], []]
+# annots = [[], []]
+annots = [[], [], [], [], [], []]
 good_out = 0
 good_in = 0
 bad_out = 0
 bad_in = 0
 for tupl in tupls:
+    # conf = tupl[0]
+    # match = tupl[1]
+    level = tupl[2]
+    annots[level].append(tupl[1])
+    # if match > 0 and match < .5:
+    #     continue
+    # if level not in [1, 2] and conf <= threshold:
+    #     if match > 0:
+    #         good_out += 1
+    #     else:
+    #         bad_out += 1
+    # else:
+    #     if match > 0:
+    #         good_in += 1
+    #     else:
+    #         bad_in += 1
+
     if tupl[1] > 0 and tupl[1] < .5:
         continue
-    if tupl[0] <= threshold:
-        stepped_match_counts[0].append(tupl[1])
+    if tupl[0] <= threshold and level in [4, 5]:
+        annots[0].append(tupl[1])
         if tupl[1] > 0:
             good_out += 1
         else:
             bad_out += 1
     else:
-        stepped_match_counts[1].append(tupl[1])
+        annots[1].append(tupl[1])
         if tupl[1] > 0:
             good_in += 1
         else:
-            bad_in += 1
+           bad_in += 1
 
 print('    \tin\tout\tΣ')
 print('good\t{}\t{}\t{}'.format(good_in, good_out, good_in+good_out))
@@ -53,6 +73,7 @@ plt.xlabel('agreement with MAG FoS');
 #     plt.hist(vals, normed=True, bins=30, alpha=.3, label=str(s))
 #plt.xlim(right=15)
 # plt.hist(stepped_match_counts, bins=25, label=list(range(5)), rwidth=2)
-plt.hist(stepped_match_counts, bins=25, label=['conf <= {}'.format(threshold), 'conf > {}'.format(threshold)], rwidth=2)
+# plt.hist(annots, bins=25, label=['conf <= {}'.format(threshold), 'conf > {}'.format(threshold)], rwidth=2)
+plt.hist(annots, bins=25, stacked=True, label=list(range(6)), rwidth=2)
 plt.legend(loc='upper right')
 plt.show()

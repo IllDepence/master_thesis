d6724d8853aa1134fc3f2ac0577eeb2adc94a0b2
Tarek Saier <tareksaier@gmail.com>
2019-01-13 23:01:35 +0100

    add plots and notes

---
+++ code/arxiv_misc/cited_doc_fields.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+s = """  mathematics: 852169
+  computer science: 293954
+  physics: 893937
+  materials science: 28943
+  business: 4229
+  engineering: 31148
+  psychology: 18322
+  medicine: 12777
+  geography: 4103
+  history: 837
+  economics: 26879
+  biology: 67194
+  political science: 2514
+  sociology: 3034
+  environmental science: 1645
+  chemistry: 78100
+  philosophy: 2172
+  geology: 18330
+  art: 858"""
+
+lines = s.split('\n')
+label_val_tups = []
+for line in lines:
+    parts = line.split(': ')
+    label_val_tups.append((parts[0].strip(), int(parts[1].strip())))
+label_val_tups = sorted(label_val_tups, key=lambda x: x[1])
+labels = []
+values = []
+vals_total = 0
+for tup in label_val_tups:
+    labels.append(tup[0])
+    values.append(tup[1])
+    vals_total += tup[1]
+print(labels)
+print(values)
+ax = plt.subplot()
+ax.barh(labels, values, .8, color='grey')
+for i, v in enumerate(values):
+    ax.text(v + 10000, i -.25, '{} ({:.2f}%)'.format(v, (v/vals_total)*100), color='black')
+plt.title('fields of study of cited docs')
+plt.xlabel('number of documents')
+plt.tight_layout()
+ax.spines['right'].set_visible(False)
+ax.get_xaxis().set_visible(False)
+plt.show()
---
+++ code/arxiv_misc/context_count_distribution_from_json.py
@@ -0,0 +1,48 @@
+""" Distribution of count of citation contexts per cited doc
+"""
+
+import json
+import os
+import re
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+with open('citing_docs_per_cited_doc.json') as f:
+    citing_counts = json.load(f)
+
+fig, ax = plt.subplots()
+# citing_count_map = {}
+# for citing_count in citing_counts:
+#     if citing_count not in citing_count_map:
+#         citing_count_map[citing_count] = 0
+#     citing_count_map[citing_count] += 1
+# 
+# x = []
+# y = []
+# for citing_count in range(max(citing_count_map.keys())):
+#     if citing_count in citing_count_map:
+#         x.append(citing_count)
+#         y.append(citing_count_map[citing_count])
+
+# plt.bar(x, y, align='center', alpha=0.5)
+# plt.plot(x, y, 'bo', markersize=1)
+# plt.hist(citing_counts, bins=500)
+y = sorted(citing_counts, reverse=True)[1:]
+x = list(range(len(citing_counts)))[1:]
+plt.plot(x, y, '-', markersize=1)
+ax.grid(color='lightgray', linestyle='--', linewidth=0.5)
+ax.set_xlabel('doc ID')
+ax.set_ylabel('citing docs')
+# ax.set_xlabel('citing docs')
+# ax.set_ylabel('cited docs')
+# plt.xticks(np.arange(0, 100, step=1))
+# plt.yticks(np.arange(0, 100000, step=10000))
+# plt.xlim(right=3000)
+# plt.xlim(left=0)
+plt.yscale('log')
+# for i, j in zip(x, y):
+#     ax.annotate(str(j), xy=(i-0.23,j+1000+(j*0.05)),
+#                 color='grey', rotation=90)
+plt.show()

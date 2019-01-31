46490c6537d30bc647223474dff25f46e6aaa2f6
Tarek Saier <tareksaier@gmail.com>
2019-01-31 16:26:41 +0100

    update figures. add notes

--- code/arxiv_misc/context_count_distribution_from_json.py
+++ code/arxiv_misc/context_count_distribution_from_json.py
@@ -7,18 +7,26 @@ import re
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from matplotlib.colors import LogNorm
+from matplotlib.pyplot import figure
 
-# with open('citing_docs_per_cited_doc.json') as f:
-#     citing_counts = json.load(f)
+font = {'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 13}
 
-with open('contexts_per_reference.json') as f:
+plt.rc('font', **font)
+
+with open('citing_docs_per_cited_doc.json') as f:
     citing_counts = json.load(f)
+
+# with open('contexts_per_reference.json') as f:
+#     citing_counts = json.load(f)
 # print(np.mean(citing_counts))
 # print(np.std(citing_counts))
 # sys.exit()
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8, 3.5))
 # citing_count_map = {}
 # for citing_count in citing_counts:
 #     if citing_count not in citing_count_map:
@@ -39,8 +47,19 @@ y = sorted(citing_counts, reverse=True)[1:]
 x = list(range(len(citing_counts)))[1:]
 plt.plot(x, y, '-', markersize=1)
 ax.grid(color='lightgray', linestyle='--', linewidth=0.5)
-ax.set_xlabel('reference ID')
-ax.set_ylabel('citation contexts')
+# ax.set_xlabel('reference ID')
+# ax.set_ylabel('citation contexts')
+ax.set_xlabel('document ID')
+ax.set_ylabel('citing documents')
+
+# manual edits
+# plt.xticks(np.arange(0, 12000001, 6000000))
+plt.xticks(np.arange(0, 2000001, 1000000))
+fmt = '{x:,.0f}'
+tick = mtick.StrMethodFormatter(fmt)
+ax.xaxis.set_major_formatter(tick)
+# ax.ticklabel_format(style='plain', scilimits=(0,7), axis='x')
+
 # ax.set_xlabel('citing docs')
 # ax.set_ylabel('cited docs')
 # plt.xticks(np.arange(0, 100, step=1))
@@ -51,4 +70,6 @@ plt.yscale('log')
 # for i, j in zip(x, y):
 #     ax.annotate(str(j), xy=(i-0.23,j+1000+(j*0.05)),
 #                 color='grey', rotation=90)
+plt.tight_layout()
+
 plt.show()

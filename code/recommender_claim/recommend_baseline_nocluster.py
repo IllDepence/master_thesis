9b4e9643d063ff8630fa3d0eb27102582d4398ff
Tarek Saier <tareksaier@gmail.com>
2019-02-21 23:09:42 +0100

    first test of using predpatt annotations for recommendation

--- code/recommender_claim/recommend_baseline_nocluster.py
+++ code/recommender_claim/recommend_baseline_nocluster.py
@@ -1,18 +1,21 @@
 """ Recommend using TFIDF/BOW approach.
 """
 
-import os
+import json
 import math
-import sys
-import random
 import operator
+import os
+import random
+import sys
 import numpy as np
 from gensim import corpora, models, similarities
 from util import bow_preprocess_string
+from scipy.sparse import csc_matrix
 from sklearn.preprocessing import MultiLabelBinarizer
 from gensim.models.ldamulticore import LdaMulticore
 # from gensim.models.ldamodel import LdaModel
 from gensim.models import LsiModel
+from gensim.matutils import corpus2csc, Sparse2Corpus
 
 
 def combine_rankings(bow_ranking, fos_boost, top_dot_prod):
@@ -29,8 +32,26 @@ def combine_rankings(bow_ranking, fos_boost, top_dot_prod):
     comb = sorted(point_dic.items(), key=operator.itemgetter(1), reverse=True)
     return [c[0] for c in comb]
 
-def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
-              combine_train_contexts=True):
+
+def sum_weighted_term_lists(wtlist, dictionary):
+    if len(wtlist) == 0:
+        return []
+    term_vecs = []
+    for weight, terms in wtlist:
+        term_vec_raw = dictionary.doc2bow(terms)
+        term_vec = [(term_id, weight*val) for term_id, val in term_vec_raw]
+        term_vecs.append(term_vec)
+    # make into numpy matrix for convenience
+    term_matrix = corpus2csc(term_vecs)
+    # calculate sum
+    sum_vec = Sparse2Corpus(
+        csc_matrix(term_matrix.sum(1))
+        )[0]
+    return sum_vec
+
+
+def recommend(docs_path, dict_path, use_fos_annot=False, pp_dict_path=None,
+              lda_preselect=False, combine_train_contexts=True):
     """ - foo
     """
 
@@ -38,9 +59,23 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
     train_mids = []
     train_texts = []
     train_foss = []
+    train_ppann = []
     foss = []
     tmp_bag = []
     adjacent_cit_map = {}
+
+    if pp_dict_path:
+        print('loading predpatt dictionary')
+        pp_dictionary = corpora.Dictionary.load(pp_dict_path)
+        pp_num_unique_tokens = len(pp_dictionary.keys())
+        use_predpatt_model = True
+        if not combine_train_contexts:
+            print(('usage of predpatt model is not implemented for not'
+                   'combining train contexts.\nexiting.'))
+            sys.exit()
+    else:
+        use_predpatt_model = False
+        pp_dictionary = None
 
     print('checking file length')
     num_lines = sum(1 for line in open(docs_path))
@@ -53,17 +88,27 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
             if idx%10000 == 0:
                 print('{}/{} lines'.format(idx, num_lines))
             cntxt_foss = []
-            if fos_annot:
-                mid, adjacent, in_doc, text, fos_annot = line.split('\u241E')
+            cntxt_ppann = []
+            # handle varying CSV formats
+            vals = line.split('\u241E')
+            if len(vals) == 4:
+                mid, adjacent, in_doc, text = vals
+            elif len(vals) == 5:
+                if use_predpatt_model:
+                    mid, adjacent, in_doc, text, pp_annot_json = vals
+                else:
+                    mid, adjacent, in_doc, text, fos_annot = vals
+            elif len(vals) == 6:
+                mid, adjacent, in_doc, text, fos_annot, pp_annot_json = vals
+            else:
+                print('input file format can not be parsed\nexiting')
+                sys.exit()
+            if len(vals) in [5, 6] and use_fos_annot:
                 cntxt_foss = [f.strip() for f in fos_annot.split('\u241F')
                               if len(f.strip()) > 0]
                 foss.extend(cntxt_foss)
-            else:
-                try:
-                    mid, adjacent, in_doc, text = line.split('\u241E')
-                except ValueError:
-                    # for evaluating w/ FoS data w/o FoS
-                    mid, adjacent, in_doc, text, unused = line.split('\u241E')
+            if use_predpatt_model:
+                cntxt_ppann = json.loads(pp_annot_json)
             # create adjacent map for later use in eval
             if mid not in adjacent_cit_map:
                 adjacent_cit_map[mid] = []
@@ -82,9 +127,12 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
                     item_in_doc = item[0]
                     item_text = item[1]
                     item_foss = item[2]
+                    item_ppann = item[3]
                     if item_in_doc not in sub_bags_dict:
                         sub_bags_dict[item_in_doc] = []
-                    sub_bags_dict[item_in_doc].append([item_text, item_foss])
+                    sub_bags_dict[item_in_doc].append(
+                        [item_text, item_foss, item_ppann]
+                        )
                 if len(sub_bags_dict) < 2:
                     # can't split, reset bag, next
                     tmp_bag = []
@@ -108,9 +156,13 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
                         test_tups.extend(sb_tup)
                     else:
                         train_tups.extend(sb_tup)
-                test.extend([
-                    [tmp_bag_current_mid, tup[0], tup[1]]
-                    for tup in test_tups])
+                test.extend(
+                    [
+                        [tmp_bag_current_mid, tup[0], tup[1],
+                         sum_weighted_term_lists(tup[2], pp_dictionary)
+                        ]
+                    for tup in test_tups
+                    ])
                 # because we use BOW we can just combine train docs here
                 if combine_train_contexts:
                     train_text_combined = ' '.join(tup[0] for tup in train_tups)
@@ -118,6 +170,12 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
                     train_texts.append(train_text_combined.split())
                     train_foss.append(
                         [fos for tup in train_tups for fos in tup[1]]
+                        )
+                    train_ppann.append(
+                        sum_weighted_term_lists(
+                            sum([tup[2] for tup in train_tups], []),
+                            pp_dictionary
+                            )
                         )
                 else:
                     for tup in train_tups:
@@ -127,7 +185,7 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
                 # reset bag
                 tmp_bag = []
                 tmp_bag_current_mid = mid
-            tmp_bag.append([in_doc, text, cntxt_foss])
+            tmp_bag.append([in_doc, text, cntxt_foss, cntxt_ppann])
     print('loading dictionary')
     dictionary = corpora.Dictionary.load(dict_path)
     num_unique_tokens = len(dictionary.keys())
@@ -138,7 +196,7 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
     # console.copen(globals(), locals())
 
     # corpora.MmCorpus.serialize('3s5min5min_corpus.mm', corpus)
-    if fos_annot:
+    if use_fos_annot:
         print('preparing FoS model')
         mlb = MultiLabelBinarizer()
         mlb.fit([foss])
@@ -153,7 +211,7 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
     if lda_preselect:
         orig_index = index.index.copy()
 
-        print('generating LDA model')
+        print('generating LDA/LSI model')
         lda = LsiModel(tfidf[corpus], id2word=dictionary, num_topics=100)
         # lda = LdaMulticore(tfidf[corpus], id2word=dictionary, num_topics=500)
         # lda = LdaModel(tfidf[corpus], id2word=dictionary, num_topics=10000)
@@ -161,6 +219,13 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
         lda_index = similarities.SparseMatrixSimilarity(
                     lda[tfidf[corpus]],
                     num_features=num_unique_tokens)
+
+
+    if use_predpatt_model:
+        print('preparing similarities')
+        pp_index = similarities.SparseMatrixSimilarity(
+            train_ppann,
+            num_features=pp_num_unique_tokens)
 
     num_cur = 0
     num_top = 0
@@ -173,7 +238,7 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
     for tpl in test:
         test_mid = tpl[0]
         test_text = bow_preprocess_string(tpl[1])
-        if fos_annot:
+        if use_fos_annot:
             test_foss_vec = mlb.transform([tpl[2]])
             sorted_dot_prod_ids = train_foss_matrix.dot(
                 test_foss_vec.transpose()
@@ -186,9 +251,14 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
                 sorted_dot_prods >= sorted_dot_prods.max()-1
                 )[0].tolist()
             top_dot_prod = sorted_dot_prods[-1]
+        if use_predpatt_model:
+            pp_sims = pp_index[tpl[3]]
+            pp_sims_list = list(enumerate(pp_sims))
+            pp_sims_list.sort(key=lambda tup: tup[1], reverse=True)
+            pp_ranking = [s[0] for s in pp_sims_list]
         test_bow = dictionary.doc2bow(test_text)
         if lda_preselect:
-            # pre select in LDA space
+            # pre select in LDA/LSI space
             lda_sims = lda_index[lda[tfidf[test_bow]]]
             lda_sims_list = list(enumerate(lda_sims))
             lda_sims_list.sort(key=lambda tup: tup[1], reverse=True)
@@ -200,9 +270,9 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
         sims_list.sort(key=lambda tup: tup[1], reverse=True)
         bow_ranking = [s[0] for s in sims_list]
         if lda_preselect:
-            # translate back from listing in LDA pick subset to global listing
+            # translate back from listing in LDA/LSI pick subset to global listing
             bow_ranking = [lda_picks[r] for r in bow_ranking]
-        if fos_annot:
+        if use_fos_annot:
             final_ranking = combine_rankings(
                 bow_ranking, fos_boost, top_dot_prod)
         else:
@@ -212,6 +282,10 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
             seen_add = seen.add
             final_ranking = [x for x in final_ranking
                      if not (train_mids[x] in seen or seen_add(train_mids[x]))]
+        if pp_sims_list[0][1] == 0:
+            final_ranking = bow_ranking
+        else:
+            final_ranking = pp_ranking
         # if top_dot_prod < 1:                # FIXME just a test
         #     continue
         # else:
@@ -270,10 +344,14 @@ def recommend(docs_path, dict_path, fos_annot=False, lda_preselect=False,
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) not in [3, 4]:
         print(('usage: python3 recommend.py </path/to/docs_file> </path/to/gen'
                'sim_dict>'))
         sys.exit()
     docs_path = sys.argv[1]
     dict_path = sys.argv[2]
-    recommend(docs_path, dict_path)
+    if len(sys.argv) == 4:
+        pp_dict_path = sys.argv[2]
+    else:
+        pp_dict_path = None
+    recommend(docs_path, dict_path, pp_dict_path=pp_dict_path)

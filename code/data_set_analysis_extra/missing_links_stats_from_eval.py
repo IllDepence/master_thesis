0aa935a1b908d9e9b68f6b2f95c26fb283c7db26
Tarek Saier <tareksaier@gmail.com>
2019-08-19 12:15:09 +0200

    complete eval of potential missing links

--- code/data_set_analysis_extra/missing_links_stats_from_eval.py
+++ code/data_set_analysis_extra/missing_links_stats_from_eval.py
@@ -29,6 +29,8 @@ with open('missing_links_eval') as f:
 
 total = valid + invalid
 
+print('--- unarXive only ---')
+print()
 print('total: {}'.format(total))
 print()
 print('general indicator of quality of "not in MAG" subset of links:')
@@ -41,3 +43,34 @@ print('  valid missing Aug 2019: {} verified -> ~{:.2f}'.format(valid-now_in_mag
 print()
 print('misc')
 print('  papers w/ 0 references in MAG: {} ({:.2f})'.format(mag0, mag0/total))
+
+mag_valid = 0
+mag_invalid = 0
+
+with open('missing_links_unarXive_eval') as f:
+    for line in f:
+        if not re.search('^\d', line):
+            # comments
+            continue
+        evl = line.strip().split(':')[-1].split('(')[0].strip()
+        if len(evl) == 0:
+            # not evalutated yet
+            continue
+        if evl[0] == 'y' or evl == 'n-':
+            mag_valid += 1
+        elif evl[0] == 'n' and not evl == 'n-':
+            mag_invalid += 1
+        elif evl[0] not in ['y', 'n']:
+            print('problem with line: "{}"'.format(line))
+            sys.exit()
+
+mag_total = mag_valid + mag_invalid
+
+print()
+print('--- MAG only ---')
+print()
+print('total: {}'.format(mag_total))
+print()
+print('general indicator of quality of "not in unarXive" subset of links:')
+print('  valid: {} ({:.2f})'.format(mag_valid, mag_valid/mag_total))
+print('  invalid: {} ({:.2f})'.format(mag_invalid, mag_invalid/mag_total))

2e01ff4cb9827e5eb8af1ffa27ba55e28b427197
Tarek Saier <tareksaier@gmail.com>
2020-01-15 10:35:43 +0100

    add significance test data and notes

---
+++ notes/improvement_significance/test.py
@@ -0,0 +1,42 @@
+import json
+import sys
+from scipy import stats
+from mlxtend.evaluate import permutation_test
+
+with open(sys.argv[1]) as f:
+    pairs = json.load(f)
+
+scores_baseline = [x[0] for x in pairs]
+scores_new = [x[1] for x in pairs]
+
+t, p = stats.ttest_rel(scores_baseline, scores_new)
+
+print('# # # Student\'t t-test # # #')
+print('t-statistic: {}'.format(t))
+print('Two-sided p-value: {}'.format(p))
+
+perm_p = permutation_test(scores_baseline, scores_new,
+                           method='approximate',
+                           num_rounds=10000,
+                           seed=0)
+print()
+print('# # # Permutation test # # #')
+print('Two-sided p-value (alpha=0.01): {}'.format(perm_p))
+
+# - - - - -
+
+num_same = 0
+num_better = 0
+num_worse = 0
+for pair in pairs:
+    if pair[1] > pair[0]:
+        num_better += 1
+    elif pair[0] > pair[1]:
+        num_worse += 1
+    else:
+        num_same += 1
+
+print()
+print('#better: {}'.format(num_better))
+print('#same: {}'.format(num_same))
+print('#worse: {}'.format(num_worse))
